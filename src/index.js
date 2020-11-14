8986492a5005ebb977ddeee01420b35b1639d47d
Francesco Lacriola <francescolacriola@MacBook-Pro-di-Francesco.local>
2020-11-14 17:32:54 +0100

    Initial Commit, Jumbotron ✅

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { GlobalStyles } from './global-styles';
+import 'normalize.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
+    <GlobalStyles />
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
+  </>,
+
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
