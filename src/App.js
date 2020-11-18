bbd1c88a20f32e850711cd47b93b4c2211d65b7e
Rensykes <chiccodallacinacolfurgone@yahoo.it>
2020-11-18 22:14:43 +0100

    Routing - initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import JumbotronContainer from './containers/jumbotron';
-import FooterContainer from './containers/footer';
-import FaqsContainer from './containers/faqs';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home, SignIn, SignUp, Browse } from './pages';
+import * as ROUTES from './constants/routes';
 
 
 export default function App() {
   return (
-    <>
-    <JumbotronContainer />
-    <FaqsContainer />
-    <FooterContainer />
-    </>
+    <Router>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
+      </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <SignUp />
+      </Route>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <SignIn />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }
