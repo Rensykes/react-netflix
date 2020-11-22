50a49533fe12baaa246acf64dc95623049dc936b
Rensykes <chiccodallacinacolfurgone@yahoo.it>
2020-11-22 22:53:41 +0100

    selectionFilter util

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {useContent} from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
     const {series} = useContent('series');
     const {films} = useContent('films');
 
+    const slides = selectionFilter({series, films});
     return (
         <div>
             <p>Hello Browse</p>
---
+++ src/utils/selection-filter.js
@@ -0,0 +1,48 @@
+export default function selectionFilter({series, films}) {
+    return {
+        series: [
+            {
+                title: 'Documentaries',
+                data: series.filter((item) => item.genre === 'documentaries')
+            },
+            {
+                title: 'Comedies',
+                data: series.filter((item) => item.genre === 'comedies')
+            },
+            {
+                title: 'Crime',
+                data: series.filter((item) => item.genre === 'crime')
+            },
+            {
+                title: 'Feel Good',
+                data: series.filter((item) => item.genre === 'feel-good')
+            },
+            {
+                title: 'Children',
+                data: series.filter((item) => item.genre === 'children')
+            },
+        ],
+        films: [
+            {
+                title: 'Drama',
+                data: films.filter((item) => item.genre === 'drama')
+            },
+            {
+                title: 'Thriller',
+                data: films.filter((item) => item.genre === 'thriller')
+            },
+            {
+                title: 'Romance',
+                data: films.filter((item) => item.genre === 'romance')
+            },
+            {
+                title: 'Suspance',
+                data: films.filter((item) => item.genre === 'suspance')
+            },
+            {
+                title: 'Children',
+                data: films.filter((item) => item.genre === 'children')
+            },
+        ]
+    }
+}
