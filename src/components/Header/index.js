e4160662a4b9eaa639479a2a56a27670261a2d89
Francesco Lacriola <francescolacriola@MacBook-Pro-di-Francesco.local>
2020-11-26 18:43:33 +0100

    Header Feature, Text, CallOut

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Background, Container, Logo, ButtonLink } from './styles/header';
-import {Link as ReactRouterLink} from 'react-router-dom';
+import { Background, Container, Logo, ButtonLink, Feature, Text, FeatureCallout } from './styles/header';
+import { Link as ReactRouterLink } from 'react-router-dom';
 
 export default function Header({ children, bg = true, ...restProps }) {
-    return bg ? <Background {...restProps}>{children}</Background>:(children)
+    return bg ? <Background {...restProps}>{children}</Background> : (children)
 }
 
 Header.Container = function HeaderContainer({ children, ...restProps }) {
@@ -24,4 +24,16 @@ Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
 
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>
+}
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+    return <Feature {...restProps}>{children}</Feature>
+}
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>;
+}
+
+Header.FeatureCallout = function HeaderFeatureCallout({ children, ...restProps }) {
+    return <FeatureCallout {...restProps}>{children}</FeatureCallout>;
 }
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,8 @@
 import React, {useState, useContext, useEffect} from 'react';
 import SelectProfileContainer from './profile';
 import {FirebaseContext} from '../context/firebase';
-import {Loading} from '../components';
+import { Loading} from '../components';
+import { Header } from '../components';
 
 export default function BrowseContainer({slides}) {
     const [profile, setProfile] = useState({});
@@ -14,12 +15,23 @@ export default function BrowseContainer({slides}) {
             setLoading(false);
         }, 2000)
     }, [profile.displayName])
+    
     return profile.displayName ? (
-            loading ? (
+
+        <>
+            {loading ? (
                 <Loading src={user.photoURL} />
             ) : (
                 <Loading.ReleaseBody /> 
-            )
+            )}
+
+            <Header src="joker1">
+                <Header.Feature>
+                    <Header.FeatureCallout>Watch Joker now</Header.FeatureCallout>
+                    <Header.Text>Pippo</Header.Text>
+                </Header.Feature>
+            </Header>
+        </>
         ) : (
             <SelectProfileContainer user={user} setProfile={setProfile} />
         )
